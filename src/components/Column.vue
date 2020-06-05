c609b86843b09cec31f696116c802717a7b3042b
Oleg Kruchay <o.kruchay@gmail.com>
2020-06-05 12:32:29 +0300

    v0.1 (MVP): Adding the following components: Column, Row and Vuex component Store.

---
+++ src/components/Column.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="column" :class="column">
+    {{ value }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cell",
+  props: {
+    value: String
+  },
+  methods: {
+    createBattleground: function (rows, cols) {
+
+    }
+  }
+};
+</script>
