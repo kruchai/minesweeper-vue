9a410fa2dd151ad0204806e889cf534461e45ef7
Oleg Kruchay <o.kruchay@gmail.com>
2020-06-17 09:40:46 +0300

    v0.4 (Beta): Cleaning up the code of main App file.

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
   <div id="app">
     <Header
       logoAlt="Minesweeper"
-      appTitle="Minesweeper on VueJS 3 (ßeta)"
+      appTitle="Minesweeper on VueJS (ßeta)"
       appUri="https://github.com/OlegScout/minesweeper-vue"
     />
+
     <div class="settings">
       <label for="setRows">Set number of rows</label>
       <input id="setRows" v-model="setNumberOfRows" type="number" placeholder="Set number of rows">
@@ -14,12 +15,8 @@
       <input id="setBombs" v-model="setNumberOfBombs" type="number" placeholder="Set number of bombs">
     </div>
 
-    <Battleground :rows="this.setNumberOfRows" :columns="this.setNumberOfColumns" :bombsPercentage="this.setNumberOfBombs" />
-
-    <!--  Start the game CTA  -->
-    <!--    <button class="btn btn-gradient start" @click="loadTheGame()">Start the game</button>-->
-    <!--  Restart CTA  -->
-    <button class="btn btn-gradient restart" @click="reloadTheGame()">Restart</button>
+    <Battleground :rows="setNumberOfRows" :columns="setNumberOfColumns" :bombsPercentage="setNumberOfBombs" />
+    <button class="btn btn-gradient restart" @click="reloadTheGame">Restart</button>
     <Footer />
   </div>
 </template>
@@ -39,17 +36,12 @@ export default {
   },
   data() {
     return {
-      message: 'Welcome to My Vue.js App',
-      status: 'Click on each cell you want',
       setNumberOfBombs: 10,
       setNumberOfRows: 10,
       setNumberOfColumns: 10
     };
   },
   methods: {
-    // loadTheGame() {
-    //   console.log('Start the game')
-    // },
     reloadTheGame() {
       location.reload();
       return false;
